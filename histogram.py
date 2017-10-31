cb364b5809de1bfcc7dd23ed86f61863ad454255
Ian Bloom <ian.bloom@gmail.com>
2017-10-31 16:37:32 -0700

    Working on frequencySort()
    Working on the frequencySort() in order to sort data into frequency bins to produce y-axis data for a histogram

--- histogram.py
+++ histogram.py
@@ -2,8 +2,10 @@ import csv, statistics, plotly
 
 # Designate the target .csv file to parse
 filename = 'alertsPerDevice.csv'
-permissions = 'rwab' # b flag may be required depending on the platform
-averageCol = 7 # location of the "average" column in our LogicMonitor report .csv
+permissions = 'rwab'  # b flag may be required depending on the platform
+
+# location of the "average" column in our LogicMonitor report .csv
+averageCol = 7
 binCount = 20
 
 # Open .csv with the designated permissions and automatically detect formatting
@@ -31,3 +33,32 @@ stats['variance'] = statistics.variance(averageValues)
 stats['max'] = max(averageValues)
 stats['min'] = min(averageValues)
 stats['binSize'] = (stats['mean'] - stats['min']) / (binCount / 2)
+
+# Consider removing duplicate entries of datapoint names (and thus histogram names) by passing it as list(set(x))
+
+# Initialize bins array by creating bincount number of entries that are stats['binSize'] apart
+bins = []
+for i in range(0,binCount):
+	bins.append(i * stats['binSize'])
+
+# This function takes an array of buckets and an array of data as input
+# and counts the number of datapoints in each bucket to produce y-axis data for a histogram
+
+def frequencySort(binArray, dataArray):
+	bins = len(binArray)
+	# Initialize frequency information to be returned
+	frequencyArray = []
+	for i in range(0,bins + 1):
+		frequencyArray.append(0)
+
+	for item in dataArray:
+		for idx, thing in enumerate(binArray):
+			# Put items from dataArray into 
+			if item >= thing and item < binArray[idx + 1]:
+				frequencyArray[idx] += 1
+			else:
+				frequencyArray[bins] += 1
+
+	return frequencyArray
+
+
